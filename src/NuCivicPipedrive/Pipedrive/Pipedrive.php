6336e8ce365988e2868f8eaad4ff90f2c95e42f6
Dan Feder <dan@nuams.com>
2015-03-13 11:53:03 -0400

    Add configuration YML file and YAML dependencies

--- src/NuCivicPipedrive/Pipedrive/Pipedrive.php
+++ src/NuCivicPipedrive/Pipedrive/Pipedrive.php
@@ -14,8 +14,13 @@ use NuCivicPipedrive\Pipedrive\Library\DealFields;
 use NuCivicPipedrive\Pipedrive\Library\Deals;
 use NuCivicPipedrive\Pipedrive\Library\PersonFields;
 use NuCivicPipedrive\Pipedrive\Library\Persons;
+use NuCivicPipedrive\Pipedrive\Library\Organizations;
+use NuCivicPipedrive\Pipedrive\Library\Products;
 use NuCivicPipedrive\Pipedrive\Library\Pipelines;
 use NuCivicPipedrive\Pipedrive\Library\Stages;
+use NuCivicPipedrive\Pipedrive\Library\Activities;
+use NuCivicPipedrive\Pipedrive\Library\Files;
+use Symfony\Component\Yaml\Parser;
 
 class Pipedrive
 {
@@ -25,6 +30,8 @@ class Pipedrive
     public $personfields;
     public $pipelines;
     public $stages;
+    public $files;
+    public $activities;
 
     /**
      * Endpoint for Pipedrive, HTTP or HTTPS (configurable).
@@ -48,23 +55,32 @@ class Pipedrive
      * Set HTTP with endpoint and token. Create classes for each API object.
      * @param string $token Pipedrive API token.
      */
-    public function __construct($token)
+    public function __construct()
     {
-        $this->token = $token;
+        $this->token = $this->getToken();
         $this->http = new HTTP($this->token, $this->endpoint);
 
         $this->deals = new Deals($this->http);
-        $this->dealfields = new DealFields($this->http);
         $this->persons = new Persons($this->http);
-        $this->personfields = new PersonFields($this->http);
+        $this->organizations = new Organizations($this->http);
+        $this->products = new Products($this->http);
         $this->pipelines = new Pipelines($this->http);
         $this->stages = new Stages($this->http);
+        $this->activities = new Activities($this->http);
+        $this->files = new files($this->http);
     }
 
     public function isAuthenticated()
     {
         $response = $this->http->get('userSettings');
         return $response->success;
+    }
+
+    private function getToken()
+    {
+        $yaml = new Parser();
+        $config = $yaml->parse(file_get_contents('app/config/config.yml'));
+        return $config['api']['token'];
     }
 
 }
